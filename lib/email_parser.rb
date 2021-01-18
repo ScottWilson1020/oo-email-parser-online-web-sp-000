9001354dd52ea19a9b4244798c79fb335dd488f5
Scott Wilson <scottwilson1020@gmail.com>
2021-01-18 21:34:53 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,15 +1,12 @@
 class EmailParser
 attr_accessor :emails
 
-def initialize(emails)
-  @emails = emails
+def initialize(email)
+  @emails = email
 end
 
 def parse
   emails.delete(',').split.uniq
 end
 
-end 
-emails2 = "asd@qw.com per@er.org per@er.org Zaza@sugar.net"
-parser2 = EmailParser.new(emails2)
-parser2.parse
+end
